b08d8c122c614edae4d50f260da03deb89a28aaf
Adam Hardy <adh167@gmail.com>
2021-05-19 16:27:42 +0100

    added file upload and download

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,41 @@
 import pandas as pd
 import streamlit as st
+import base64 
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
+# create csv downloader
+def csv_downloader(data, filename):
+	csvfile = data.to_csv()
+	b64 = base64.b64encode(csvfile.encode()).decode()
+	href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download as CSV</a>'
+	st.markdown(href,unsafe_allow_html=True)
 
-#================= Pre Processing ================================
+#================================ File Upload ================================
+
+st.header("Upload Playwaze Reports")
+
+playwaze_teams_expected_filename = "Playwaze teams.xlsx"
+playwaze_teams_uploaded_file = st.file_uploader("Upload Playwaze Teams File",type=['xlsx'])
+if playwaze_teams_uploaded_file is not None:
+    file_details = {"FileName":playwaze_teams_uploaded_file.name,"FileType":playwaze_teams_uploaded_file.type,"FileSize":playwaze_teams_uploaded_file.size}
+    if file_details["FileName"] != playwaze_teams_expected_filename:
+        st.error(f"File name must be: '{playwaze_teams_expected_filename}'")
+        st.stop()
+else:
+    st.stop()
+
+playwaze_members_expected_filename = "Playwaze team members.xlsx"
+playwaze_members_uploaded_file = st.file_uploader("Upload Playwaze Members File",type=['xlsx'])
+if playwaze_members_uploaded_file is not None:
+    file_details = {"FileName":playwaze_members_uploaded_file.name,"FileType":playwaze_members_uploaded_file.type,"FileSize":playwaze_members_uploaded_file.size}
+    if file_details["FileName"] != playwaze_members_expected_filename:
+        st.error(f"File name must be: '{playwaze_members_expected_filename}'")
+        st.stop()
+else:
+    st.stop()
+
+#================================ Pre Processing ================================
 
 col_event = "Event"
 col_crew_name = "Crew Name"
@@ -10,7 +43,7 @@ col_crew_members = "Rowers"
 col_verified = "Verified"
 
 #load teams
-df_teams = pd.read_excel("Playwaze teams.xlsx")
+df_teams = pd.read_excel(playwaze_teams_uploaded_file)
 df_playwaze_teams = df_teams.rename(columns={
     "Entry type":col_event,
     "Entry name":col_crew_name,
@@ -19,7 +52,7 @@ df_playwaze_teams = df_teams.rename(columns={
     })
 
 #load rowers
-df_playwaze_rowers = pd.read_excel("Playwaze team members.xlsx")
+df_playwaze_rowers = pd.read_excel(playwaze_members_uploaded_file)
 df_playwaze_rowers = df_playwaze_rowers.rename(columns={
     "Team":col_crew_name,
     "Team type": col_event
@@ -44,9 +77,6 @@ df_playwaze_rowers = df_playwaze_rowers.append(df_coxes, ignore_index=True)
 df_playwaze_rowers = df_playwaze_rowers.sort_values(by=["Event", "Crew Name", "Position"])
 
 
-
-
-
 df_entries = df_playwaze_teams.loc[df_playwaze_teams.duplicated(subset=col_crew_name) == False].sort_values(by=[col_event, col_crew_name])
 
 #extract number of seats
@@ -68,13 +98,17 @@ rowers_display_columns = ["Event", "Crew Name", "Position", "Name"]
 
 #================= Web Page ================================
 
-st.title("Scottish Rowing Junior Summer Regatta")
-
 st.header("Entries")
-st.write(df_entries[team_display_columns])
+df = df_entries[team_display_columns]
+st.write(df)
+csv_downloader(df, "entries.csv")
 
 st.header("Rowers")
-st.write(df_playwaze_rowers[rowers_display_columns].pivot(index=[col_event, col_crew_name], columns="Position", values="Name"))
+df = df_playwaze_rowers[rowers_display_columns].pivot(index=[col_event, col_crew_name], columns="Position", values="Name")
+st.write(df)
+csv_downloader(df, "rowers.csv")
 
 st.header("Events")
-st.write(df_events)
+df = df_events
+st.write(df)
+csv_downloader(df, "events.csv")
